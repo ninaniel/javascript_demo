1b693a62d86c8fbad99de372756ddf17c3b55478
Nino Shubitidze <shubitidzenina@gmail.com>
2026-04-14 15:16:36 +0400

    filter falsy values

---
+++ bouncer.js
@@ -0,0 +1,18 @@
+/* You should have a bouncer function that takes an array as argument. 
+The bouncer function should return a new array that contains 
+the same elements as the array passed in as argument with the falsy elements removed. 
+The bouncer function should not change the array passed in as an argument.
+*/
+
+
+function bouncer(arr) {
+  const result = [];
+
+  for (let item of arr) {
+    if (item) {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
